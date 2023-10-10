5eb2eec4afb5122923d58b0bab6b968534ea3d89
SantiagoBarletta <santiagobarletta@outlook.com>
2023-10-10 19:39:58 -0300

    clase 12

---
+++ js/script3.js
@@ -0,0 +1,24 @@
+/////////////// BUCLES /////////////////
+/////WHILE/////
+
+// let i = 0
+// while (i <= 15){
+//     document.write("El valor de i es: " + i +"<br>");
+//     i = i +1;
+// }
+////// OTRO WHILE////
+
+let numero = 100;
+while (numero > 0){
+document.write("Sólo quedan: " + numero +" pasos por caminar<br>");
+numero = numero - 1;
+}
+
+////// DO WHILE ///////
+
+// let i = 5;
+// do { document.write("El valor de i es: " + i +"<br>");
+// i = i +1;
+// }
+// while (i <= 15);
+
