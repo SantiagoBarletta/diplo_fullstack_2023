95bc89a03976842ccb60d656c226a7da06097d58
SantiagoBarletta <santiagobarletta@outlook.com>
2023-10-20 13:59:42 -0300

    practica

--- js/script9.js
+++ js/script9.js
@@ -1,10 +1,10 @@
 class Pelicula {
-    constructor(titulo, poster, sinopsis,trailer) {
-      this.titulo = titulo;
-      this.poster = poster;
-      this.sinopsis = sinopsis;
-      this.trailer = trailer;
-    }
+  constructor(titulo, poster, sinopsis, trailer) {
+    this.titulo = titulo;
+    this.poster = poster;
+    this.sinopsis = sinopsis;
+    this.trailer = trailer;
+  }
 }
 
 let boton = document.getElementById("Agregar")
@@ -13,27 +13,28 @@ let input2 = document.getElementById("Poster")
 let input3 = document.getElementById("Sinopsis")
 let input4 = document.getElementById("Trailer")
 let div = document.getElementById("divPelis")
-let arrayPeli =[]
+let arrayPeli = []
 
-boton.addEventListener('click', () => 
-{
-    arrayPeli.push(new Pelicula(input.value, input2.value, input3.value, input4.value))    
-    arrayPeli.forEach(Pelicula => {
+boton.addEventListener('click', () => {
+  arrayPeli.push(new Pelicula(input.value, input2.value, input3.value, input4.value))
+
+  for (const pelicula of arrayPeli) {
         div.innerHTML += `
-        <h2 class="tituloPelicula">${Pelicula.titulo}</h2>
-        <img src="${Pelicula.poster}" class="poster" />
-        <div class="sinopsis">${Pelicula.sinopsis}</div>
-        <div class="trailer">
-          <h3 class="cabezaTrailer">Trailer</h3>
-          <iframe src="${Pelicula.trailer}" title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen></iframe>
-        </div>
-        `
-    });
-    input.value=""
-    input2.value=""
-    input3.value=""
-    input4.value=""
+           <h2 class="tituloPelicula">${pelicula.titulo}</h2>
+           <img src="${pelicula.poster}" class="poster" />
+           <div class="sinopsis">${pelicula.sinopsis}</div>
+           <div class="trailer">
+             <h3 class="cabezaTrailer">Trailer</h3>
+             <iframe src="${pelicula.trailer}" title="YouTube video player"
+               frameborder="0"
+               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+               allowfullscreen></iframe>
+           </div>
+           `
+  }
+  input.value = ""
+  input2.value = ""
+  input3.value = ""
+  input4.value = ""
+
 })
