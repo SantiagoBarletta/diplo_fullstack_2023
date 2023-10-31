dc6c6a82ba3e871b89ff4a752e05d07eed1b9f16
SantiagoBarletta <santiagobarletta@outlook.com>
2023-10-31 19:11:30 -0300

    actualizacion clase 17

--- js/scriptclase17.js
+++ js/scriptclase17.js
@@ -1,53 +1,65 @@
+
 class Mascota {
-    constructor(nombre, raza){
-        this.nombre = nombre;
-        this.raza = raza;
-        this.adoptada = false;
-    }
+  constructor(nombre, raza) {
+    this.nombre = nombre;
+    this.raza = raza;
+    this.adoptada = false;
+  }
 
-// Metodo para marcar mascota como adoptada
-adoptar(){
+  // Metodo para marcar mascota como adoptada
+  adoptar() {
     this.adoptada = true;
+  }
 }
-}
+
 //Funcion para agregar una nueva mascota a la lista
+function agregarMascota() {
+  const inputMascota = document.getElementById("nombreMascota");
+  const input2Mascota = document.getElementById("razaMascota");
+  const nombreMascota = inputMascota.value.trim();
+  const razaMascota = input2Mascota.value.trim();
 
-function  agregarMascota() {
-    const inputMascota = document.getElementById("nombreMascota");
-    const input2Mascota = document.getElementById("razaMascota");
-    const nombreMascota = inputMascota.value.trim()  ;
-    const razaMascota = input2Mascota.value.trim();
-    
-    if (nombreMascota !== ""){
-        const nuevaMascota = new Mascota(nombreMascota, razaMascota);
-        // Crear un elemento de la lista y aplicar estilo segun estado
-        const elementoLista = document.createElement("li");
-        elementoLista.textContent = "Nombre de la Mascota: " + nuevaMascota.nombre + " | Raza: " + nuevaMascota.raza;
+  if (nombreMascota !== "") {
+    const nuevaMascota = new Mascota(nombreMascota, razaMascota);
 
-        if (nuevaMascota.adoptada){
-            elementoLista.classList.add("adoptada");
-        }
+    // Crear un elemento de la lista y aplicar estilo segun estado
+    const elementoLista = document.createElement("li");
+    elementoLista.textContent =
+      "Nombre de la Mascota: " +
+      nuevaMascota.nombre +
+      " | Raza: " +
+      nuevaMascota.raza;
 
-        //agregar un evento de click para marcar/adoptar la mascota
-
-        elementoLista.addEventListener("click", function(){
-            nuevaMascota.adoptar();
-            elementoLista.classList.add("adoptada");
-        })
-
-        // Agregar la mascota
-
-        const listaMascotas = document.getElementById("listaMascotas");
-        listaMascotas.appendChild(elementoLista);
-        //Limpia campo entrada
-        inputMascota.value =""
-        input2Mascota.value= "";
+    if (nuevaMascota.adoptada) {
+      elementoLista.classList.add("adoptada");
     }
 
+    //agregar un evento de click para marcar/adoptar la mascota
+    elementoLista.addEventListener("click", function () {
+      nuevaMascota.adoptar();
+      elementoLista.classList.add("adoptada");
+    });
+
+    // Agregar la mascota
+    const listaMascotas = document.getElementById("listaMascotas");
+    listaMascotas.appendChild(elementoLista);
+
+    //Limpia campo entrada
+    inputMascota.value = "";
+    input2Mascota.value = "";
+  }
 }
 
 // Obtener boton para agregar mascotas
+const botonAgregarMascota = document.getElementById("agregarMascota");
 
-    const botonAgregarMascota = document.getElementById("agregarMascota");
-    // agrega un evento para que haga click
-    botonAgregarMascota.addEventListener("click", agregarMascota);
+// agrega un evento para que haga click
+botonAgregarMascota.addEventListener("click", agregarMascota);
+
+// Evento para el campo entrada (permitir agregar precionando ENTER)
+const input2Mascota = document.getElementById("razaMascota");
+input2Mascota.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    agregarMascota();
+  }
+});
